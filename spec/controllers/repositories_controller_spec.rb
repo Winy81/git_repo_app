e4ee0f16c8cc5376da306800d3150471e826b78f
Winy81 <Adam.Nemeth@FootballPools.com>
2023-08-19 20:40:27 +0100

    Add Repositories#search controller test

--- spec/controllers/repositories_controller_spec.rb
+++ spec/controllers/repositories_controller_spec.rb
@@ -4,4 +4,30 @@ require 'rails_helper'
 
 RSpec.describe RepositoriesController, type: :controller do
 
+  describe 'POST #search' do
+
+    context 'with search results - github_search' do
+
+      it 'returns a successful response with one item' do
+
+        allow_any_instance_of(Net::HTTP).to receive(:get).and_return
+
+        post :search, params: { search_term: 'test-repo' }
+
+        expect(response).to have_http_status(200)
+
+      end
+    end
+
+    context 'without search results - main page' do
+      
+      it 'returns a successful response with no items' do
+
+        expect(Net::HTTP).to_not receive(:get)
+
+        post :search
+
+      end
+    end
+  end
 end
